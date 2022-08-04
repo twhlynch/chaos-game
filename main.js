1b96223b2b9c6ecda0e34469ce60ea1bf12aa3b2
Tom Lynch <twhlynch@gmail.com>
2022-08-04 12:08:31 +1000

    debugging

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ if ((window.innerWidth > 0) && (window.innerHeight > 0)) {
 else {
     c.width = screen.width*10;
     c.height = screen.height*10;
-    document.body.innerHTML = screen.width;
 }
+
+document.body.innerHTML = toString(screen.width) + toString(window.innerWidth);
+
 function drawDot(x, y, r=1) {
     ctx.fillRect(x,y,r,r);
     instances.push([x, y]);
