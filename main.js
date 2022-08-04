12949eaa499fca0248bc5ff65c4aa66e9074e9e4
Tom Lynch <twhlynch@gmail.com>
2022-08-04 12:16:52 +1000

    quality

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var c = document.getElementById("render");
 var ctx = c.getContext("2d");
 
-c.width = $(document).width();
-c.height = $(document).height();
+c.width = $(document).width()*10;
+c.height = $(document).height()*10;
 //document.body.innerHTML = toString(screen.width) + toString(window.innerWidth);
 
 function drawDot(x, y, r=1) {
